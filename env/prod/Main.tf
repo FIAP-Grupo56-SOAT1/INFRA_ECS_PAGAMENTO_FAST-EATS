70d38444c9c68f1ab958b5561f9fdd2109f76301
wilian.penaforte@vetta.digital <wilian.penaforte@vetta.digital>
2024-01-28 12:50:02 -0300

    criação do projeto

---
+++ env/prod/Main.tf
@@ -0,0 +1,25 @@
+
+# Criando um modulo que utiliza os dados do infra para criação do ambiente
+module "prod" {
+  source                  = "../../infra"
+  containerDbName         = jsondecode(data.aws_secretsmanager_secret_version.mysql_credentials.secret_string)["dbname"]
+  containerDbUser         = jsondecode(data.aws_secretsmanager_secret_version.mysql_credentials.secret_string)["username"]
+  containerDbPassword     = jsondecode(data.aws_secretsmanager_secret_version.mysql_credentials.secret_string)["password"]
+  containerDbRootPassword = jsondecode(data.aws_secretsmanager_secret_version.mysql_credentials.secret_string)["password"]
+  containerDbServer       = jsondecode(data.aws_secretsmanager_secret_version.mysql_credentials.secret_string)["host"]
+  containerDbPort         = jsondecode(data.aws_secretsmanager_secret_version.mysql_credentials.secret_string)["port"]  
+}
+
+
+#obteando dados do secret manager
+data "aws_secretsmanager_secret" "mysql" {
+  name = "prod/soat1grupo56/MySQLPagamento"
+}
+
+data "aws_secretsmanager_secret_version" "mysql_credentials" {
+  secret_id = data.aws_secretsmanager_secret.mysql.id
+}
+
+output "IP_alb" {
+  value = module.prod.IP
+}
---
+++ infra/variaveis.tf
@@ -0,0 +1,95 @@
+variable "nome_repositorio" {
+  type    = string
+  default = "repositorio-terraform"
+}
+variable "cargoIam" {
+  type    = string
+  default = "fasteats"
+}
+
+variable "perfil" {
+  type    = string
+  default = "fasteats"
+}
+
+variable "portaAplicacao" {
+  type    = number
+  default = 8080
+}
+
+variable "ambiente" {
+  type    = string
+  default = "fasteats"
+}
+
+variable "containerPort" {
+  type    = number
+  default = 8080
+}
+
+variable "containerName" {
+  type    = string
+  default = "fasteats"
+}
+
+variable "imagemDocker" {
+  type    = string
+  default = "fiap56soat1/fasteats-ms-pedido-api:latest"
+}
+
+variable "containerDbServer" {
+  type = string
+}
+
+variable "containerDbPort" {
+  type    = string
+  default = "3306"
+}
+
+variable "containerDbName" {
+  type = string
+}
+
+variable "containerDbUser" {
+  type = string
+}
+
+variable "containerDbPassword" {
+  type = string
+}
+
+variable "containerDbRootPassword" {
+  type = string
+}
+
+variable "containerMercadoPagoEmailEmpresa" {
+  type    = string
+  default = "pagamento@lanchonete-fiap.com.br"
+}
+
+
+variable "containerMercadoPagoCredential" {
+  type    = string
+  default = "TEST-2087963774082813-080820-ee2b9b80edbdecf3ea8453bb8c088bc7-64946408"
+}
+
+
+variable "containerMercadoPagoUderId" {
+  type    = string
+  default = "64946408"
+}
+
+variable "containerMercadoPagoTipoPagamento" {
+  type    = string
+  default = "pix"
+}
+
+variable "URL_PEDIDO_SERVICE" {
+  type    = string
+  default = "http://localhost:8082"
+}
+
+variable "URL_COZINHA_PEDIDO_SERVICE" {
+  type    = string
+  default = "http://localhost:8083"
+}
